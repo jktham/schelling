5e7c5d1447987021344e6f4975cf4b2d85d7f57b
BenjaGramsch <57944945+BenjaGramsch@users.noreply.github.com>
2023-12-04 15:14:28 +0100

    modified:   code/model_combined.py

--- code/model_combined.py
+++ code/model_combined.py
@@ -30,6 +30,8 @@ class Model:
 		self.iteration = 0
 		self.strategy_weights = strategy_weights
 		self.empty_ratio = empty_ratio
+		self.history_satisfaction_expensive = [0]*iterations
+		self.history_satisfaction_cheap = [0]*iterations
 		self.history_satisfaction = [0]*iterations
 		self.history_time = [0]*iterations
 
@@ -46,6 +48,7 @@ class Model:
 
 		self.points = np.empty(shape=(point_types), dtype=object)
 		self.point_types = point_types
+        
 
 	# fill agent grid according to types and ratios
 	def generate_agents(self):
@@ -64,7 +67,7 @@ class Model:
 					self.agents[x, y] = Agent(
 						type=t,
 						threshold=self.agent_thresholds[t],
-						wealth=self.agent_wealths[t] if t != 4 else 2,
+						wealth=self.agent_wealths[t] if t != 3 else 8,
 						interests=self.agent_interests[t]
 					)
 
@@ -126,20 +129,28 @@ class Model:
 			return 0.0
 		return similar_neighbors / (len(neighbors) - empty_neighbors)
     
+    # get neighbors as list of coordinate tuples around (x, y)
+	def get_price(self, x, y):
+		return [(i, j) for i in range(max(0, x-2), min(x+3, self.size)) for j in range(max(0, y-2), min(y+3, self.size)) if (i, j) != (x, y)]
+
+    
     # Change price of cells
 	def update_price(self):
 		for x in range(self.size):
 			for y in range(self.size):
-				neighbors = self.get_neighbors(x, y)
-				self.cells[x,y].price = 8 - sum([(self.agents[neighbor].type == -1) for neighbor in neighbors])
+				neighbors = self.get_price(x, y)
+				self.cells[x,y].price = 24 - sum([(self.agents[neighbor].type == -1) for neighbor in neighbors])
 
-
+                
 	# check if agent at (x, y) is satisfied or not
 	def is_unsatisfied(self, x, y):
 		if self.agents[x, y].type == -1:
 			return False
 		satisfaction = self.get_satisfaction(x, y)
-		return satisfaction < self.agents[x, y].threshold
+		if self.strategy_weights["min_price"] > 0.0:
+			return (satisfaction < self.agents[x, y].threshold) and (self.agents[x, y].wealth == self.cells[x,y].price)
+		else:
+   			return (satisfaction < self.agents[x, y].threshold)         
 	
 	# get desirability score of cell for given agent, depending on strategy weights
 	def get_desirability(self, cell, agent):
@@ -193,7 +204,11 @@ class Model:
 	def run(self):
 		t0 = time.time()
 		for i in range(self.iterations):
+			self.history_satisfaction_expensive[i] = self.get_average_satisfaction_expensive()
+			self.history_satisfaction_cheap[i] = self.get_average_satisfaction_cheap()
 			self.history_satisfaction[i] = self.get_average_satisfaction()
+
+
 			self.history_time[i] = round(time.time()-t0, 2)
 
 			if i % 10 == 0:
@@ -205,67 +220,109 @@ class Model:
 			self.iterate()
 
 	# get average satisfaction of all agents
-	def get_average_satisfaction(self):
+	def get_average_satisfaction_expensive(self):
 		sat = 0
+		high_income_agent = 0.00001
 		for x in range(self.size):
 			for y in range(self.size):
-				if self.agents[x, y].type != -1:
+				if (self.agents[x, y].type != -1) and (self.cells[x,y].price > 19):
 					sat += self.get_satisfaction(x, y)
-		return sat / self.size**2
+					high_income_agent += 1
+		return sat / high_income_agent
+    
+    
+	# get average satisfaction of all agents
+	def get_average_satisfaction_cheap(self):
+		sat = 0
+		low_income_agent = 0
+		for x in range(self.size):
+			for y in range(self.size):
+				if (self.agents[x, y].type != -1) and (self.cells[x,y].price < 19):
+					sat += self.get_satisfaction(x, y)
+					low_income_agent += 1
+		return sat / low_income_agent
+    
+    # get average satisfaction of all agents
+	def get_average_satisfaction(self):
+		sat = 0
+		low_income_agent = 0
+		for x in range(self.size):
+			for y in range(self.size):
+				if (self.agents[x, y].type != -1):
+					sat += self.get_satisfaction(x, y)
+					low_income_agent += 1
+		return sat / low_income_agent
+    
+    
+    
 	
 	# display current state of model using matplotlib
 	def display(self):
+        
 		fig = plt.figure()
 		fig.suptitle(f'Schelling Model ({self.iteration} iterations)')
 
-		fig.add_subplot(4, 2, 1)
+		fig.add_subplot(5, 2, 1)
 		plt.imshow(np.vectorize(lambda a: a.type)(self.agents), cmap='cool', vmin=-1, vmax=self.agent_types)
 		plt.title(f'Agent Types')
 
-		fig.add_subplot(4, 2, 2)
+		fig.add_subplot(5, 2, 2)
 		plt.imshow(np.vectorize(lambda a: a.wealth)(self.agents), cmap='cool', vmin=0, vmax=max(self.agent_wealths))
 		plt.title(f'Agent Wealth')
 
-		fig.add_subplot(4, 2, 3)
+		fig.add_subplot(5, 2, 3)
 		plt.imshow(np.vectorize(lambda c: c.type)(self.cells), cmap='cool', vmin=0, vmax=self.cell_types)
 		plt.title(f'Cell Types')
 
-		fig.add_subplot(4, 2, 4)
+		fig.add_subplot(5, 2, 4)
 		plt.imshow(np.vectorize(lambda c: c.price)(self.cells), cmap='cool', vmin=0, vmax=max(self.agent_wealths))
 		plt.title(f'Cell Prices')
 
-		fig.add_subplot(4, 2, 5)
+		fig.add_subplot(5, 2, 5)
 		plt.imshow(np.vectorize(lambda c: c.distances[0])(self.cells), cmap='cool', vmin=0, vmax=70)
 		plt.title(f'Cell Distances 0')
 
-		fig.add_subplot(4, 2, 6)
+		fig.add_subplot(5, 2, 6)
 		plt.imshow(np.vectorize(lambda c: c.distances[1])(self.cells), cmap='cool', vmin=0, vmax=70)
 		plt.title(f'Cell Distances 1')
 
-		fig.add_subplot(4, 2, 7)
-		plt.plot(range(0, self.iterations), self.history_satisfaction)
-		plt.title(f'Average Satisfaction')
+		fig.add_subplot(5, 2, 7)
+		plt.plot(range(0, self.iterations), self.history_satisfaction_expensive)
+		plt.title(f'Average satisfaction expensive places')
+        
+		fig.add_subplot(5, 2, 8)
+		plt.plot(range(0, self.iterations), self.history_satisfaction_cheap)
+		plt.title(f'Average satisfaction cheap places')
 
-		fig.add_subplot(4, 2, 8)
+
+		fig.add_subplot(5, 2, 9)
+		plt.plot(range(0, self.iterations), self.history_satisfaction)
+		plt.title(f'Average satisfaction')
+        
+		fig.add_subplot(5, 2, 10)
 		plt.plot(range(0, self.iterations), self.history_time)
 		plt.title(f'Simulation Time')
+                
+        
 		plt.savefig("plot.png")
 		plt.show()
 
+
+
 # example model
 model = Model(
-	size=50,
+	size=25,
 	iterations=300,
 	strategy_weights={
-		"random": 0,
+		"random": 1,
 		"min_price": 0,
-		"min_point_dist": 1
+		"min_point_dist": 0
 	},
-	empty_ratio=0.4,
-	agent_types=4,
-	agent_ratios=[0.25, 0.25, 0.25, 0.25],
-	agent_thresholds=[0.7, 0.7, 0.7, 0.7, 0.7],
-	agent_wealths=[4, 5, 6, 7, 8],
+	empty_ratio=0.3,
+	agent_types= 3,
+	agent_ratios=[1/3, 1/3, 1/3],
+	agent_thresholds=[0.5, 0.5, 0.5, 0.5, 0.5],
+	agent_wealths=[16, 18, 20, 22, 24],
 	cell_types=4,
 	cell_ratios=[0.4, 0.3, 0.2, 0.1],
 	point_types=2,
